7cd85924f59c585232b1eab376ba6d380c495a57
Jan Weiß <jan@geheimwerk.de>
2017-04-12 14:37:38 +0200

    Example #1.

--- state-machines/main.c
+++ state-machines/main.c
@@ -6,10 +6,62 @@
 //  Copyright © 2017 Jan. All rights reserved.
 //
 
+#include <stdbool.h>
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <time.h>
+#include <unistd.h>
 
-int main(int argc, const char * argv[]) {
-	// insert code here...
-	printf("Hello, World!\n");
-    return 0;
+const long millisecond = 1000000L;
+
+void millisecond_sleep(long milliseconds) {
+	nanosleep((const struct timespec[]){0, (milliseconds * millisecond)}, NULL);
+}
+
+void emit(const char *string) {
+	printf("%s"
+		   "\r", // Move the cursor back to the start of the current line. CLI support required.
+		   string);
+	
+	fflush(stdout); // Force the output to be displayed.
+}
+
+void led_on() {
+	emit(" ☀︎ ");
+}
+
+void led_off() {
+	emit(" ◦ ");
+}
+
+typedef enum {
+	LED_ON,
+	LED_OFF,
+} state_t;
+
+int main(int argc, const char *argv[]) {
+	state_t state = LED_OFF;
+	
+	while (true) {
+		switch (state) {
+			case LED_OFF:
+				led_on();
+				state = LED_ON;
+				break;
+				
+			case LED_ON:
+				led_off();
+				state = LED_OFF;
+				break;
+				
+			default:
+				exit(EXIT_FAILURE);
+				break;
+		}
+		
+		millisecond_sleep(200);
+	}
+	
+	return EXIT_FAILURE; // Unreachable.
 }
